a3147742be272a2822b25beebb67ca49769c1360
Cyrille Ahmed MIDINGOYI <midingoyi@cirad.fr>
2024-07-10 07:05:21 +0200

    update

---
+++ src/modfilegen/Converter/DssatConverter/dssatcultivarconverter.py
@@ -0,0 +1,34 @@
+from modfilegen.converter import Converter
+from sqlite3 import Connection
+import os
+import pandas as pd
+import traceback
+import shutil
+
+class DssatCultivarConverter(Converter):
+    def __init__(self):
+        super().__init__()
+
+    def export(self, directory_path, master_input_connection, pltfolder, usmdir):
+        ST = directory_path.split(os.sep)   
+        sq = """SELECT CropManagement.idMangt as idMangt, ListCultOption.PRCROP as crop 
+        FROM (ListCultOption INNER JOIN (ListCultivars INNER JOIN CropManagement ON ListCultivars.IdCultivar = CropManagement.Idcultivar) ON ListCultOption.CodePSpecies = ListCultivars.CodePSpecies) where idMangt= '%s' ;"""%(ST[-1])   
+        df_sim = pd.read_sql(sq, master_input_connection)
+        rows = df_sim.to_dict('records')
+        
+        src_path_cul = os.path.join(pltfolder, rows[0]["crop"]+"CER047.CUL")
+        src_path_eco = os.path.join(pltfolder, rows[0]["crop"]+"CER047.ECO")
+        src_path_spe = os.path.join(pltfolder, rows[0]["crop"]+"CER047.SPE")
+        
+        if not os.path.exists(usmdir):
+            os.makedirs(usmdir)
+        shutil.copy(src_path_cul, usmdir)
+        shutil.copy(src_path_eco, usmdir)
+        shutil.copy(src_path_spe, usmdir)
+        return rows[0]["crop"]
+        #return src_path_cul, src_path_eco, src_path_spe
+
+
+
+
+
