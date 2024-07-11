3bb4c9466ebc571652aa8bf9c900231ede168a01
cyrillemidingoyi <ahmedmidingoyi@yahoo.fr>
2024-07-11 23:23:33 +0200

    update

--- src/modfilegen/Converter/DssatConverter/dssatrun.sh
+++ src/modfilegen/Converter/DssatConverter/dssatrun.sh
@@ -4,15 +4,21 @@ OLD_PWD="${PWD}"
 
 USM_DIR="$1"
 input_dir="$2"
+dt=$3
 
 cd "$USM_DIR"
 
-dssat B DSSBatch.v47  > /dev/null
+dssat B DSSBatch.v47  #> /dev/null
 
 base=$(basename "$USM_DIR")
 if [ -f "Summary.OUT" ]; then
     mv Summary.OUT "$input_dir/Summary_$base.OUT"
 fi
-rm -rf "$USM_DIR"
+
+# if dt=1, then delete the USM_DIR
+if [ $dt -eq 1 ]; then
+    rm -rf "$USM_DIR"
+fi
+
 cd "$OLD_PWD"
 
